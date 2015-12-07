6bcf01c34abd9090885ee4120dce2741dcefe0f2
Traun Leyden <tleyden@couchbase.com>
2015-12-06 17:46:22 -0800

    Remove generic cobra text

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,6 @@
 // Copyright ©2015 NAME HERE <EMAIL ADDRESS>
 //
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//     
+//
 package cmd
 
 import (
@@ -23,22 +23,15 @@ import (
 	"github.com/spf13/viper"
 )
 
-
 var cfgFile string
-
 
 // This represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "deepstyle",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your application. For example:
-
-Cobra is a Cli library for Go that empowers applications. This
-application is a tool to generate the needed files to quickly create a Cobra
-application.`,
-// Uncomment the following line if your bare application has an action associated with it
-//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Use DeepLearning to generate art images",
+	Long:  `Kick off neural-style torch.  See https://github.com/tleyden/deepstyle`,
+	// Uncomment the following line if your bare application has an action associated with it
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 //Execute adds all child commands to the root command sets flags appropriately.
@@ -54,14 +47,13 @@ func init() {
 	cobra.OnInitialize(initConfig)
 	// Here you will define your flags and configuration settings
 	// Cobra supports Persistent Flags which if defined here will be global for your application
-	
-  RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.deepstyle.yaml)")
-	
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.deepstyle.yaml)")
+
 	// Cobra also supports local flags which will only run when this action is called directly
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle" )
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 }
-
 
 // Read in config file and ENV variables if set.
 func initConfig() {
@@ -70,12 +62,11 @@ func initConfig() {
 	}
 
 	viper.SetConfigName(".deepstyle") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AddConfigPath("$HOME")      // adding home directory as first search path
+	viper.AutomaticEnv()              // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
-
